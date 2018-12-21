deb9253f09c023d4fc7011fbaaf507236067daea
bennyziio <bapjoo2@gmail.com>
2018-12-21 17:51:17 +0900

    Updates

---
+++ precache-manifest.81c1798805f0ca7efbc8b227d1596695.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "71c53fcc876ab1ed7965",
+    "url": "/jj-flower/static/js/runtime~main.71c53fcc.js"
+  },
+  {
+    "revision": "744251ce7543b3dc1192",
+    "url": "/jj-flower/static/js/main.744251ce.chunk.js"
+  },
+  {
+    "revision": "517fee201af13b75943b",
+    "url": "/jj-flower/static/js/1.517fee20.chunk.js"
+  },
+  {
+    "revision": "9c2fef5c0b65c9e73a2155de52429b22",
+    "url": "/jj-flower/index.html"
+  }
+];
